3cd2d2e3264f6a46c8c86e4a92f96c615ea8a8af
David Dantas <daviddantasjunior@gmail.com>
2021-06-10 14:17:26 -0300

    feat: Starting microservices and configuring the rabbitmq

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, Body } from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
 
-@Controller()
+@Controller('api/v1/hello')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private clientNestjs: ClientProxy;
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor() {
+    this.clientNestjs = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://guest:guest@127.0.0.1:5672/'],
+        queue: 'nestjs-gateway',
+      },
+    });
+  }
+
+  @Post()
+  async postHello(@Body() hello: string) {
+    return await this.clientNestjs.emit('post-hello', hello);
   }
 }
