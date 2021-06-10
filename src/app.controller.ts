f3b11cefbd9bca403fba8705daf0fcbae55a31c6
David Dantas <daviddantasjunior@gmail.com>
2021-06-10 14:00:18 -0300

    chore: Configuring .editorconfig, Eslint with Prettier and support to husky

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,12 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import {
-  ClientProxy,
-  ClientProxyFactory,
-  Transport,
-} from '@nestjs/microservices';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
-@Controller('api/v1/hello')
+@Controller()
 export class AppController {
-  private clientCnt: ClientProxy;
+  constructor(private readonly appService: AppService) {}
 
-  constructor() {
-    this.clientCnt = ClientProxyFactory.create({
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqp://guest:guest@127.0.0.1:5672/'],
-        queue: 'cnt-gateway',
-      },
-    });
-  }
-
-  @Post()
-  async postHello(@Body() hello: string) {
-    return await this.clientCnt.emit('post-hello', hello);
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
